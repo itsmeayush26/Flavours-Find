691d733844cbd7c091a889d46187648fca02e420
itsmeayush26 <ayushsingh0255@gmail.com>
2025-03-28 12:37:13 +0530

    random commit part 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,39 @@
-import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
+const loggedInUser = () => {
+  // API call to check authentication
+  return false;
+};
+
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+    />
+  </a>
+);
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="header">
-      <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
-      </div>
+      <Title />
       <div className="nav-items">
         <ul>
           <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>About</li>
+          <li>Contact</li>
           <li>Cart</li>
         </ul>
       </div>
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
     </div>
   );
 };
