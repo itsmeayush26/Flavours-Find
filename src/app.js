80c8681653be327e33507cfe10fcc7aae52b4554
itsmeayush26 <ayushsingh0255@gmail.com>
2025-04-05 01:06:45 +0530

    building app day 08

--- src/app.js
+++ src/app.js
@@ -1,25 +1,59 @@
-import "../index.css"; // Adjust the path based on location
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestrauntMenu";
+import Profile from "./components/Profile";
 
 const AppLayout = () => {
   return (
-    <div className="app">
+    <>
       <Header />
-      <Body />
-    </div>
+      <Outlet />
+      <Footer />
+    </>
   );
 };
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/about", // parentPath/{path} => localhost:1244/about
+        element: <About />,
+        children: [
+          {
+            path: "profile", // parentPath/{path} => localhost:1244/about/profile
+            element: <Profile />,
+          },
+        ],
+      },
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
-
+root.render(<RouterProvider router={appRouter} />);
 
 // import React from "react";
 // import ReactDOM from "react-dom/client";
